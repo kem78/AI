f9f9822c9d880ab950d2407b950f25a736cead0d
kem78 <152094485+kem78@users.noreply.github.com>
2023-11-27 03:52:24 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,18 @@
+let 신호 = ""
+serial.redirect(
+SerialPin.USB_TX,
+SerialPin.USB_RX,
+BaudRate.BaudRate9600
+)
+basic.forever(function () {
+    신호 = serial.readString()
+    if (신호 == "1") {
+        basic.showIcon(IconNames.Scissors)
+    }
+    if (신호 == "2") {
+        basic.showIcon(IconNames.Ghost)
+    }
+    if (신호 == "3") {
+        basic.showIcon(IconNames.Pitchfork)
+    }
+})
